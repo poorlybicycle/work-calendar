782afc6c0c673bc2b879a48031279f451ce4bb4b
Mime Jr <petravidovic8@gmail.com>
2022-09-19 22:59:43 -0400

    comment on js, add let declaration

--- calendar.js
+++ calendar.js
@@ -1,3 +1,4 @@
+//Displays current date in the jumbotron
 var today = moment();
 $('#currentDay').text(today.format('LL'));
 
@@ -5,3 +6,17 @@ var saveContent = document.querySelectorAll('#saveBtn');
 var textContent = document.querySelectorAll('#inputBox');
 var showList = JSON.parse(localStorage.getItem('showList') || "[]");
 
+var currentDate = new Date();
+var currentHour = currentDate.getHours();
+
+const rows = document.getElementsByClassName("row");
+
+//For each row (timeblock), make the time an integer to compare to later.
+Array.from(rows).forEach(row => {
+    let
+        rowIdString = row.id,
+        rowHour;
+    if (rowIdString) {
+        rowHour = parseInt(rowIdString);
+    }
+})
