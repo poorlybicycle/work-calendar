d748f3573cc4291bdb6541b879624d6be88057fd
Mime Jr <petravidovic8@gmail.com>
2022-09-19 22:42:32 -0400

    add variables to js

--- calendar.js
+++ calendar.js
@@ -1,3 +1,7 @@
 var today = moment();
 $('#currentDay').text(today.format('LL'));
 
+var saveContent = document.querySelectorAll('#saveBtn');
+var textContent = document.querySelectorAll('#inputBox');
+var showList = JSON.parse(localStorage.getItem('showList') || "[]");
+
